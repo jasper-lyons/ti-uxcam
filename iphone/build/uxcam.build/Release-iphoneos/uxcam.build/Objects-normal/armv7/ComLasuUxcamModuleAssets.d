d9963d46ca87962ac5c43c40de1442f69582aad1
Jasper Lyons <jasperlyons@Jaspers-MacBook-Pro.local>
2015-01-13 21:01:49 +0000

    Initial commit for UXCam titanium module

---
+++ iphone/build/uxcam.build/Release-iphoneos/uxcam.build/Objects-normal/armv7/ComLasuUxcamModuleAssets.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/jasperlyons/workspace/projects/lasu/uxcam/iphone/Classes/ComLasuUxcamModuleAssets.m \
+  /Users/jasperlyons/workspace/projects/lasu/uxcam/iphone/Classes/ComLasuUxcamModuleAssets.h
